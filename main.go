0f6cae1aa6f1b12a7d4ef35200c67b6e8b6b2c72
Yulia Yakina <juilia.yakina190@gmail.com>
2026-05-26 15:59:34 +0300

    add menu feature with go map

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    menu := map[string]float64 {
+        "soup": 4.99,
+        "pie": 7.99,
+        "salad": 6.99,
+        "toffee pudding": 3.55,
+    }
+
+    fmt.Println(menu)
+    fmt.Println(menu["pie"])
+}
